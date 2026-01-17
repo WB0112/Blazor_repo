1819c38018065f4b3522acf87a29a55c08385749
santuparsi <santuparsi@gmail.com>
2026-01-17 15:14:54 +0530

    commit117_2

---
+++ HandsOn/Day-3/HandsOnOrderManagementDashboard/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace HandsOnOrderManagementDashboard.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string Status { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
