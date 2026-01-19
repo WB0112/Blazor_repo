727e5cd08bea3d4ffbf487ab3932313f5efed335
santuparsi <santuparsi@gmail.com>
2026-01-19 19:48:35 +0530

    commit119_1

---
+++ HandsOn/Day-4/OrderManagementDashBoard_1/Models/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderManagementDashBoard.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public string Status { get; set; } = "Pending";
+        public string Remakrs { get; set; } = string.Empty;
+    }
+}
---
+++ HandsOn/Day-4/OrderManagementDashBoard_1/Services/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using OrderManagementDashBoard.Models;
+
+namespace OrderManagementDashBoard.Services
+{
+    public class OrderService
+    {
+        public event Action<Order>? OrderUpdated;
+        public void AddOrder(List<Order> orders, Order order)
+        {
+            orders.Add(order);
+            OrderUpdated?.Invoke(order);
+        }
+        public void UpdateStuaus(Order order,string status)
+        {
+            order.Status = status;
+            OrderUpdated?.Invoke(order);
+        }
+        public void UpdateRemakrs(Order order,string remark)
+        {
+            order.Remakrs= remark;
+            OrderUpdated?.Invoke(order);
+        }
+    }
+}
