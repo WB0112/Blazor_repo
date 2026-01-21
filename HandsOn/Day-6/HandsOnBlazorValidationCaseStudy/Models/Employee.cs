5e58694b7f98d6c2f6342ffbc7f8d77f3b795077
santuparsi <santuparsi@gmail.com>
2026-01-21 19:55:30 +0530

    commit121_3

---
+++ HandsOn/Day-6/HandsOnBlazorValidationCaseStudy/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using HandsOnBlazorValidationCaseStudy.Validators;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+namespace HandsOnBlazorValidationCaseStudy.Models
+{
+    public class Employee
+    {
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress]
+        [CompanyEmail]
+        public string Email { get; set; }
+        [RegularExpression("[6-9]{1}[0-9]{9}",ErrorMessage ="Invalid Mobile Number")]
+        public string Mobile { get; set; }
+
+        [Range(21, 60, ErrorMessage = "Age must be between 21 and 60")]
+        public int Age { get; set; }
+            
+      
+    }
+
+}
