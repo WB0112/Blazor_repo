cd76a8274a4c1b6573f07a7c28ae2decb181ef83
santuparsi <santuparsi@gmail.com>
2026-01-22 19:42:18 +0530

    commit122_2

---
+++ HandsOn/Day-7/HandsOnBlazorNavigationAndRouting/Services/AppState.cs
@@ -0,0 +1,8 @@
+﻿namespace HandsOnBlazorNavigationAndRouting.Services
+{
+    public class AppState
+    {
+        public bool IsAdmin { get; set; } = false;
+        public bool IsUser { get; set; } = false;    
+    }
+}
