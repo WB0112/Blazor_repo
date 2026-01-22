6b1df55be06e3146958d93e67d7baedc617f7ba0
santuparsi <santuparsi@gmail.com>
2026-01-22 18:42:42 +0530

    commit122_1

---
+++ HandsOn/Day-7/HandsOnBlazorNavigationAndRouting/Services/UserService.cs
@@ -0,0 +1,30 @@
+﻿using HandsOnBlazorNavigationAndRouting.Models;
+using Microsoft.AspNetCore.SignalR.Protocol;
+namespace HandsOnBlazorNavigationAndRouting.Services
+{
+    public class UserService
+    {
+        private readonly List<Models.User> _users = new()
+        {
+            new Models.User { Id = 1, Name = "Alice", Email = "alice@gmail.com", Role = "Admin", Password = "password1" },
+            new Models.User { Id = 2, Name = "Bob", Email = "bob@gmail.com", Role = "User", Password = "password1" },
+            new Models.User { Id = 3, Name = "Charlie", Email = "charlie@gmail.com", Role = "User", Password = "password1" }
+        };
+        public void Register(User user)
+        {
+            _users.Add(user);
+        }
+        public User? Authenticate(string email, string password)
+        {
+            return _users.FirstOrDefault(u => u.Email == email && u.Password == password);
+        }
+        public User? GetUser(string email)
+        {
+            return _users.FirstOrDefault(u => u.Email == email);
+        }
+        public List<User> GetUsers()
+        {
+            return _users;
+        }   
+    }
+}
