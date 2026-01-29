d64f4bc63caa31efe18bc2ef46ee6e3560eea281
santuparsi <santuparsi@gmail.com>
2026-01-29 17:28:32 +0530

    commit129_4

---
+++ HandsOn/Day-12/BlazorConsumeWebComponent/BlazorConsumeWebComponent/wwwroot/js/fancy-box.js
@@ -0,0 +1,11 @@
+﻿class FancyBox extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML=`
+        <div style="border: 2px solid blue; padding: 10px; margin: 10px;">
+            <h2>Fancy Box Component</h2>
+            <p>This is a fancy box created using a Web Component!</p>
+        </div>
+        `; 
+    }
+}
+customElements.define('fancy-box', FancyBox);
